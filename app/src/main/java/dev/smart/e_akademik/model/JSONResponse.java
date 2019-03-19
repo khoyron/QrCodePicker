dce9bd6cf83a24b8ec872d013d9c1a7540060bea
Andre Setiawan A <ahmadiandre@gmail.com>
2019-03-19 10:02:39 +0700

    Initial commit

---
+++ app/src/main/java/dev/smart/e_akademik/model/JSONResponse.java
@@ -0,0 +1,13 @@
+package dev.smart.e_akademik.model;
+
+/**
+ * Created by Fnatic Anxdre on 3/31/2018.
+ */
+
+public class JSONResponse {
+        private User[] android;
+
+        public User[] getAndroid() {
+            return android;
+        }
+    }

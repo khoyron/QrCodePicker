52a48ede43f32ba64ded4fc992a9be3bf29636d6
khoiron <m.khoiron07@gmail.com>
2019-10-25 22:33:25 +0700

    add class QrcodePicker
    - add class Qrcode picker
    - implement QrcodePicker in MainActivity

--- app/src/main/java/dev/qrcode/picker/MainActivity.kt
+++ app/src/main/java/dev/qrcode/picker/MainActivity.kt
@@ -2,11 +2,35 @@ package dev.qrcode.picker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.qrcodepicker.QrcodePicker
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        qrcode_picker.callbackScanner(object : QrcodePicker.CallbackScannerListener{
+            override fun responseScanner(result: String?) {
+                Toast.makeText(this@MainActivity,result,Toast.LENGTH_LONG).show()
+            }
+        } )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        qrcode_picker.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        qrcode_picker.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        qrcode_picker.onResume()
     }
 }
---
+++ qrcodepicker/src/main/java/com/example/qrcodepicker/QrcodePicker.kt
@@ -0,0 +1,123 @@
+package com.example.qrcodepicker
+
+import android.widget.LinearLayout
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import me.dm7.barcodescanner.zbar.Result
+import me.dm7.barcodescanner.zbar.ZBarScannerView
+
+class QrcodePicker : LinearLayout, View.OnClickListener,ZBarScannerView.ResultHandler {
+
+    lateinit var callback : CallbackScannerListener
+
+    lateinit var mScannerView: ZBarScannerView
+
+    fun callbackScanner(callbackListener: CallbackScannerListener){
+        callback = callbackListener
+    }
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    @JvmOverloads
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int = 0) : super(context, attrs, defStyle) {
+
+        init()
+    }
+
+    private fun init() {
+        setOrientation(VERTICAL)
+        View.inflate(context, R.layout.scanner_barcode_view, this)
+
+        initViewScanner()
+    }
+
+    private fun initViewScanner() {
+        val contentFrame = findViewById(R.id.frame_scanner) as ViewGroup
+
+        mScannerView = ZBarScannerView(context)
+        contentFrame.addView(mScannerView)
+
+        checkPermissionCamera(context)
+    }
+
+    val READ_REQUEST_CODE = 897
+
+    private fun checkPermissionCamera(context: Context) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (context.checkSelfPermission(Manifest.permission.CAMERA) !== PackageManager.PERMISSION_GRANTED ) {
+                (context as Activity).requestPermissions(arrayOf( Manifest.permission.CAMERA), READ_REQUEST_CODE)
+
+            } else {
+                startScanner()
+            }
+        }
+        else{
+            startScanner()
+        }
+    }
+
+
+    fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        when(requestCode){
+            READ_REQUEST_CODE ->{
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startScanner()
+                }
+                else{
+                    showAlert("Sorry","Please allow camera permision",context)
+                }
+            }
+        }
+    }
+
+    private fun showAlert(title: String, message: String, activity: Context) {
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton("Ok") { dialog, which -> dialog.dismiss() }
+        builder.create().show()
+    }
+
+
+    private fun startScanner() {
+        mScannerView.setResultHandler(this) // Register ourselves as a handler for scan results.
+        mScannerView.startCamera()          // Start camera on resume
+    }
+
+
+    private fun stopCamera() {
+        mScannerView.stopCamera()
+    }
+
+    fun onResume() {
+        startScanner()
+    }
+
+    fun onPause() {
+        stopCamera()
+    }
+
+    interface CallbackScannerListener{
+        fun responseScanner(result: String?)
+    }
+
+    override fun onClick(v: View?) {
+
+    }
+
+    override fun handleResult(rawResult: Result?) {
+        callback.responseScanner(rawResult?.contents)
+    }
+
+
+}
